3fec36794960b2de65deec7112dcb80a6b456408
Vinoth2305 <vino23feb@gmail.com>
2024-12-24 01:04:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/multi-form-task-siam/",
-});
+export default {
+  base: '/multi-form-task-siam/',  // Add the subdirectory path here
+};

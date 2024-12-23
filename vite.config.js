ec62f7465ae89e93ee8f779efe189ded16870949
Vinoth kumar <vinofeb23@gmail.com>
2024-12-23 23:47:33 +0530

    Update vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mdx from "@mdx-js/rollup";
 
 export default defineConfig({
-  plugins: [
-    { enforce: "pre", ...mdx() },
-    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
-  ],
+  plugins: [react()],
+  base: "/multi-form-task-siam/",
+
+  optimizeDeps: {
+    include: ["console"],
+  },
 });
